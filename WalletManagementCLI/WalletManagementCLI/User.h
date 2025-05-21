3e58cedbbf09a060c1d0b3c2f30adbc7fe48e893
huongnguyen92 <88789059+huongnguyen92@users.noreply.github.com>
2025-05-21 17:09:33 +0700

    Feat/user  (#2)
    * Update User
    
    * AddNewUser
    GetAllUsers
    loginUser
    Connection string
    
    ---------
    
    Co-authored-by: nguyenhuong123xt@gmail.com <huongnguyen92>

--- WalletManagementCLI/WalletManagementCLI/User.h
+++ WalletManagementCLI/WalletManagementCLI/User.h
@@ -1,5 +1,32 @@
 #pragma once
-ref class User
+#using <System.Data.dll>
+
+#include <string>
+#include <vector>
+#include <chrono>
+
+using namespace System;
+using namespace System::Data;
+using namespace System::Data::SqlClient;
+
+
+
+class User
 {
+public:
+  std::int32_t userId;
+  std::string userName;
+  std::string password;
+  std::string role;
+  std::chrono::system_clock::time_point lastLogin;
+
+  User(std::int32_t id, std::string name, std::string pass, std::string userRole);
+  User(std::string name, std::string pass, std::string userRole);
+
+  static std::string generatePassword();
+  bool AddNewUser();
+  static std::vector<User> GetAllUsers();
+  static bool Login(std::string userName, std::string password);
 };
 
+extern User* loggedInUser;
