945a42818757219e664744fda709bf34607cfc55
nguyenhuong123xt@gmail.com <huongnguyen92>
2025-05-21 16:40:43 +0700

    Update User

--- WalletManagementCLI/WalletManagementCLI/User.h
+++ WalletManagementCLI/WalletManagementCLI/User.h
@@ -1,5 +1,20 @@
 #pragma once
-ref class User
+#include <string>
+
+#include <chrono>
+class User
 {
+public:
+  std::int32_t userId;
+  std::string userName;
+  std::string password;
+  std::string role;
+  std::chrono::system_clock::time_point lastLogin;
+
+  User(std::int32_t id, std::string name, std::string pass, std::string userRole);
+  User(std::string name, std::string pass, std::string userRole);
+
+  static std::string generatePassword();
+  bool AddNewUser();
 };
 
