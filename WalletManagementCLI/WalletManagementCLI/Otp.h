4b5cf9b416bf29ff57eea877be676053aea8e341
khoinh2122 <hoangkhoi8609@gmail.com>
2025-05-21 17:09:24 +0700

    OTP

--- WalletManagementCLI/WalletManagementCLI/Otp.h
+++ WalletManagementCLI/WalletManagementCLI/Otp.h
@@ -1,5 +1,18 @@
-#pragma once
-ref class Otp
-{
+#ifndef OTP_H
+#define OTP_H
+
+#include <string>
+#include <chrono>
+
+class OTP {
+public:
+  int otpId;
+  std::string otpCode;
+  int userId;
+  bool isUsed;
+  std::chrono::system_clock::time_point expireTime;
+
+  OTP(int id, std::string code, int uId, bool used, std::chrono::system_clock::time_point time);
 };
 
+#endif
