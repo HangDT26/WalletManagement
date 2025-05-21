d7e9eac08f672a7bb1c722e6d34651396edea115
HangDT26 <handgth@gmail.com>
2025-05-21 15:58:25 +0700

    Code base

---
+++ WalletManagementCLI/WalletManagementCLI/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"WalletManagementCLI")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"Administrator")];
+[assembly:AssemblyProductAttribute(L"WalletManagementCLI")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) Administrator 2025")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+
+[assembly:ComVisible(false)];
---
+++ WalletManagementCLI/WalletManagementCLI/WalletManagementCLI.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "User.h"
+#include "Wallet.h"
+#include "Transaction.h"
+#include "DatabaseManager.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    return 0;
+}
